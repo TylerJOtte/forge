7c9f4e1dcd68e061962e7ea8ea8acafc027e2964
Tyler J. Otte <TylerJOtte@icloud.com>
2021-03-14 17:16:21 -0500

    Add ElementsError model
    This provides an enum for a throwable `Elements` error.

---
+++ src/Forge/Forge/ElementsError.swift
@@ -0,0 +1,24 @@
+//=============================================================================//
+//                                                                             //
+//  ElementsError.swift                                                        //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 3/14/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import Foundation
+
+/// An throwable `Elements Error`
+public enum ElementsError: Error {
+    
+    case isEmpty
+    case isFull
+}
