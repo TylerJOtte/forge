7890a7f0cfb65cf798925aea883bb2777afe5f2d
Tyler J. Otte <TylerJOtte@icloud.com>
2021-03-14 16:14:27 -0500

    Add default Deck is empty test
    This provides a unit test that ensurs a default Deck of Cards is empty.

---
+++ src/Forge/ForgeTests/Cards/DeckTests.swift
@@ -0,0 +1,32 @@
+//=============================================================================//
+//                                                                             //
+//  DeckTests.swift                                                                 //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 3/14/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import XCTest
+@testable import Forge
+
+/// Unit tests for the `Deck` class.
+class DeckTests: XCTestCase {
+    
+    /// Tests that a default deck is empty.
+    func testDefaultDeckIsEmpty() {
+        
+        // Given
+        let deck: Deck = Deck()
+        
+        // When/Then
+        XCTAssert(deck.isEmpty())
+    }
+}
