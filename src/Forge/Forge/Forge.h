c57df08a7a9caa172627307416592e4f7e6c5354
Tyler J. Otte <TylerJOtte@icloud.com>
2021-03-14 13:55:09 -0500

    Add Xcode framework project to workspace
    This adds the initial configuration files for the framework and tests.

---
+++ src/Forge/Forge/Forge.h
@@ -0,0 +1,18 @@
+//
+//  Forge.h
+//  Forge
+//
+//  Created by Tyler J. Otte on 3/14/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Forge.
+FOUNDATION_EXPORT double ForgeVersionNumber;
+
+//! Project version string for Forge.
+FOUNDATION_EXPORT const unsigned char ForgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Forge/PublicHeader.h>
+
+
