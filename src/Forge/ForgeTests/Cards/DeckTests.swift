87d205d067eaf498fa8eb1e3acee50f802ca1334
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-02 16:09:33 -0500

    Add Deck count increments by one with added Card test
    This provides a unit test that ensures the count of a Deck is
    incremented by one after adding a Card to it.

--- src/Forge/ForgeTests/Cards/DeckTests.swift
+++ src/Forge/ForgeTests/Cards/DeckTests.swift
@@ -143,6 +143,26 @@ class DeckTests: XCTestCase {
         XCTAssert(deck.count < deck.maxCards)
     }
     
+    /// Tests that the count of a`Deck` with`Card`s increments by one after adding a `Card` to it.
+    func test_count_withAddedCard_IncrementsByOne() throws {
+        
+        // Given
+        let title1 = "Card 1"
+        let title2 = "Card 2"
+        let card1 = Card(title1)
+        let card2 = Card(title2)
+        let cards = [card1]
+        let deck = Deck(of: cards)!
+        let expected = 2
+        
+        // When
+        try deck.add(card2)
+        let actual = deck.count
+        
+        // Then
+        XCTAssertEqual(expected, actual)
+    }
+    
     //-------------------------------------------------------------------------//
     //                                 Full Deck                               //
     //-------------------------------------------------------------------------//
