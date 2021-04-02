9b8985aebad03d9a7c2b261e3e458d62854b8022
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-02 18:41:07 -0500

    Add Suit model
    This provides an enum for a standard French-suit playing Card suit.

---
+++ src/Forge/Forge/Card/PlayingCards/Suit.swift
@@ -0,0 +1,26 @@
+//=============================================================================//
+//                                                                             //
+//  Suit.swift                                                                 //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 4/02/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import Foundation
+
+/// A standard French-suited `PlayingCard` suit.
+public enum suit {
+    
+    case clubs
+    case diamonds
+    case hearts
+    case spades
+}
