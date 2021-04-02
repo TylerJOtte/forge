410f5730cbeb7f27c45823eb75196a46d2118214
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-02 18:40:10 -0500

    Add Rank model
    This provides an enum for a hierarchical position.

---
+++ src/Forge/Forge/Rank.swift
@@ -0,0 +1,35 @@
+//=============================================================================//
+//                                                                             //
+//  Rank.swift                                                                 //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 4/02/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import Foundation
+
+/// A hierarchical position.
+public enum Rank {
+    
+    case ace
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case ten
+    case jack
+    case queen
+    case king
+}
