32089cfd527c7eedda9d6e9e4c882a95ace36ea5
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-03 10:06:08 -0500

    Migrate Rank to Descriptions namespace
    This creates a new namespace for description features and moves the Rank
    enum into it.

--- src/Forge/Forge/Features/Descriptions/Rank.swift
+++ src/Forge/Forge/Features/Descriptions/Rank.swift
@@ -17,19 +17,41 @@
 import Foundation
 
 /// A hierarchical position.
-public enum Rank {
+public enum Rank: Int {
+    
+    //=========================================================================//
+    //                                 ATTRIBUTES                              //
+    //=========================================================================//
     
     case ace
-    case two
-    case three
-    case four
-    case five
-    case six
-    case seven
-    case eight
-    case nine
-    case ten
+    case one = 1
+    case two = 2
+    case three = 3
+    case four = 4
+    case five = 5
+    case six = 6
+    case seven = 7
+    case eight = 8
+    case nine = 9
+    case ten = 10
     case jack
     case queen
     case king
+    case joker
+    
+    //=========================================================================//
+    //                                  METHODS                                //
+    //=========================================================================//
+
+    /// Determines if the `Rank` is a numeric `Rank` between the given range.
+    ///
+    /// - Parameters:
+    ///   - min: The minimum numeric `Rank` possible.
+    ///   - max: The maximum numeric `Rank` possible.
+    ///
+    /// - Returns: True if the`Rank` is a numeric `Rank` between the given range.
+    public func isNumeric(from min: Int = 1, to max: Int = 10) -> Bool {
+
+        return self.rawValue >= min && self.rawValue <= max
+    }
 }
