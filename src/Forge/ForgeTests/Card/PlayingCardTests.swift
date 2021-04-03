6a2c1c11497188c51982548897a332cfbfe8d024
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-03 13:50:56 -0500

    Add are equal PlayingCards test
    This provides a unit test that ensures two PlayingCards with the same
    Rank and Suit are equal.

--- src/Forge/ForgeTests/Card/PlayingCardTests.swift
+++ src/Forge/ForgeTests/Card/PlayingCardTests.swift
@@ -1,9 +1,9 @@
 //=============================================================================//
 //                                                                             //
-//  CardTests.swift                                                            //
+//  PlayingCardTests.swift                                                     //
 //  Forge                                                                      //
 //                                                                             //
-//  Created by Tyler J. Otte on 3/14/21.                                       //
+//  Created by Tyler J. Otte on 4/03/21.                                       //
 //-----------------------------------------------------------------------------//
 //                                                                             //
 // This source file is part of the Forge framework project.                    //
@@ -15,33 +15,24 @@
 //=============================================================================//
 
 import XCTest
+import SwiftUI
 @testable import Forge
 
-/// Unit tests for the `Card` class.
-class CardTests: XCTestCase {
+/// Unit tests for the `PlayingCard` class.
+class PlayingCardTests: XCTestCase {
     
-    /// Tests that two `Card`s with the same titles are equal.
-    func test_areEqual_sameTitles_true() {
+    /// Tests that two standard`PlayingCard`s with the same `Rank` & `Suit` are equal.
+    func test_areEqual_sameRankAndSuit_true() {
         
         // Given
-        let title = "Card"
-        let card1 = Card(title)
-        let card2 = Card(title)
+        let rank = Rank.ace
+        let symbol = Symbol.clover
+        let color = Color.black
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank, of: suit)
+        let card2 = PlayingCard(rank, of: suit)
         
         // When/Then
         XCTAssertEqual(card1, card2)
-    }
-    
-    /// Tests that two `Card`s with different titlte are not equal.
-    func testAreNotEqual_DifferentTitles_True() {
-        
-        // Given
-        let title1 = "Card 1"
-        let title2 = "Card 2"
-        let card1 = Card(title1)
-        let card2 = Card(title2)
-        
-        // When/Then
-        XCTAssertNotEqual(card1, card2)
     }
 }
