b5180aadd7b4ebb112dc41c962b123fd1df74470
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-10 14:15:40 -0500

    Add PlayingCard sequential order test method
    This provides an extension method that determines if a given set of
    PlayingCards are in sequential order.

--- src/Forge/Forge/Extensions/ArrayExtension.swift
+++ src/Forge/Forge/Extensions/ArrayExtension.swift
@@ -15,10 +15,44 @@
 //=============================================================================//
 
 /// An extension for common `Array` operations.
-extension Array where Element: Equatable {
+extension Array where Element: Comparable {
     
-    func areUnique() -> Bool {
+    /// Determines if all items in the collection are equal.
+    ///
+    /// - Precondition: None.
+    /// - Postcondition: None.
+    /// - Returns: True if all items in the collection are equal, else false.
+    func areEqual() -> Bool {
         
         return !self.contains(where: {$0 != self.first})
     }
+    
 }
+
+/// An extension for common `PlayingCard Array` operations.
+extension Array where Element: PlayingCard  {
+
+    /// Determines if all `Card`s are in sequential order.
+    ///
+    /// - Precondition: None.
+    /// - Postcondition: None.
+    /// - Returns: True if all `Card`s in the collection are in sequantial order else false.
+    func areSequential() -> Bool {
+        
+        let lastCard = self.count - 1
+        var areSequential = true
+        var card = 0
+        
+        while (areSequential && card < lastCard) {
+            
+            let cardNextRank = self[card].rank.next
+            let nextCardRank = self[card + 1].rank
+            
+            areSequential = cardNextRank == nextCardRank
+            card += 1
+        }
+        
+        return areSequential
+    }
+}
+
