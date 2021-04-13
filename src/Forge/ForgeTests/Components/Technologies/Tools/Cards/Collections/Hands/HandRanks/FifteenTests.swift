9e48f0ed9a3839436397ed7d0241eec8a1ed0693
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-12 21:43:14 -0500

    Add init Fifteen with less than 15 points sum test
    This provies a unit test that ensures creating a Fifteen HandRank with
    PlayingCards that have points that sum to less than 15 returns nil.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/FifteenTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/FifteenTests.swift
@@ -49,4 +49,33 @@ class FifteenTests: XCTestCase {
         // Then
         XCTAssert(fifteen != nil)
     }
+    
+    //-------------------------------------------------------------------------//
+    //                               Failure                                   //
+    //-------------------------------------------------------------------------//
+    
+                               //            //
+                               // Points Sum //
+                               //            //
+    
+    /// Tests that creating a `Fifteen HandRank` with `PlayingCard`s that have points that sum to
+    /// less than 15 returns nil.
+    func test_init_withUnder15PointsSum_returnsNil() {
+        
+        // Given
+        let rank1 = Rank.ten
+        let rank2 = Rank.four
+        let color = Color.black
+        let symbol = Symbol.clover
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank1, of: suit)!
+        let card2 = PlayingCard(rank2, of: suit)!
+        let cards = [card1, card2]
+        
+        // When
+        let fifteen = Fifteen(of: cards)
+        
+        // Then
+        XCTAssert(fifteen == nil)
+    }
 }
