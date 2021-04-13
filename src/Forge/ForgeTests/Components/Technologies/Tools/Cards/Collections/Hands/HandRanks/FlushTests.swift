3e35576bf73d52cc810f208b26468b38ef0cbd3d
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-13 18:03:58 -0500

    Add init Flush HandRank with excessive Cards test
    This provides a unit test that ensures creating a Flush HandRank with
    more than five PlayingCards returns nil.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/FlushTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/FlushTests.swift
@@ -85,4 +85,32 @@ class FlushTests: XCTestCase {
         // Then
         XCTAssert(flush == nil)
     }
+    
+    /// Tests that creating a `Flush HandRank` with more than five `PlayingCards` returns nil.
+    func test_init_withExcessiveCards_returnsNil() {
+
+        // Given
+        let rank1 = Rank.one
+        let rank2 = Rank.two
+        let rank3 = Rank.three
+        let rank4 = Rank.four
+        let rank5 = Rank.five
+        let rank6 = Rank.six
+        let color = Color.black
+        let symbol = Symbol.clover
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank1, of: suit)!
+        let card2 = PlayingCard(rank2, of: suit)!
+        let card3 = PlayingCard(rank3, of: suit)!
+        let card4 = PlayingCard(rank4, of: suit)!
+        let card5 = PlayingCard(rank5, of: suit)!
+        let card6 = PlayingCard(rank6, of: suit)!
+        let cards = [card1, card2, card3, card4, card5, card6]
+        
+        // When
+        let flush = Flush(of: cards)
+        
+        // Then
+        XCTAssert(flush == nil)
+    }
 }
