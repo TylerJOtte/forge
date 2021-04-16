fa1cca43dde1597a703d8a7453de1723dce3a307
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-15 21:57:11 -0500

    Add expected triple Run title test
    This provides a unit test that ensures the title of a triple Run equals
    "Triple Run".

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/RunTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/RunTests.swift
@@ -351,4 +351,32 @@ class RunTests: XCTestCase {
         // Then
         XCTAssertEqual(expected, actual)
     }
+    
+    /// Tests that the title of a triple `Run HandRank` equals "Triple `Run`".
+    func test_title_ofTripleRun_equalsTripleRun() {
+        
+        let rank1 = Rank.one
+        let rank2 = Rank.two
+        let rank3 = Rank.three
+        let rank4 = Rank.three
+        let rank5 = Rank.three
+        let color = Color.black
+        let symbol = Symbol.clover
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank1, of: suit)!
+        let card2 = PlayingCard(rank2, of: suit)!
+        let card3 = PlayingCard(rank3, of: suit)!
+        let card4 = PlayingCard(rank4, of: suit)!
+        let card5 = PlayingCard(rank5, of: suit)!
+        let cards = [card1, card2, card3, card4, card5]
+        let pairs = 3
+        let tripleRun = Run(of: cards, with: pairs)!
+        let expected = "Triple Run"
+        
+        // When
+        let actual = tripleRun.title
+        
+        // Then
+        XCTAssertEqual(expected, actual)
+    }
 }
