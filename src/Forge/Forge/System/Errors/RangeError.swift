a5ea0eb6d6fb8e92b0331da4de4d9d9ad5fd73b2
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-17 17:15:11 -0500

    Add RangeError model
    This provdies an enum for a throwable range error.

---
+++ src/Forge/Forge/System/Errors/RangeError.swift
@@ -0,0 +1,24 @@
+//=============================================================================//
+//                                                                             //
+//  RangeError.swift                                                           //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 4/17/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import Foundation
+
+/// An throwable `Range Error`
+public enum RangeError: Error {
+    
+    case invalidMin
+    case invalidMax
+}
