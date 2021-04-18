6ade77b03a913e500ea73e8d1c946f9c49a16cfb
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-18 08:54:59 -0500

    Add expected TripleRun pairs count
    This provides a unit test that ensures the # of pairs in a TripleRun
    equals three.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/TripleRunTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/TripleRunTests.swift
@@ -224,4 +224,31 @@ class TripleRunTests: XCTestCase {
         XCTAssertEqual(expected, actual)
     }
     
+    /// Tests that the # of pairs in a`TripleRun` equals three
+    func test_pairs_ofTripleRun_equalsThree() throws {
+        
+        // Given
+        let rank1 = Rank.one
+        let rank2 = Rank.two
+        let rank3 = Rank.three
+        let rank4 = Rank.three
+        let rank5 = Rank.three
+        let color = Color.black
+        let symbol = Symbol.clover
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank1, of: suit)!
+        let card2 = PlayingCard(rank2, of: suit)!
+        let card3 = PlayingCard(rank3, of: suit)!
+        let card4 = PlayingCard(rank4, of: suit)!
+        let card5 = PlayingCard(rank5, of: suit)!
+        let cards = [card1, card2, card3, card4, card5]
+        let tripleRun = try TripleRun(of: cards)
+        let expected = 3
+        
+        // When
+        let actual = tripleRun.pairs
+        
+        // Then
+        XCTAssertEqual(expected, actual)
+    }
 }
