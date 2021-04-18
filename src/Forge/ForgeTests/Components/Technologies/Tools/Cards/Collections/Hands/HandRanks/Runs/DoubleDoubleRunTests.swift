dae52981eb26f4e41c13507162b9bae6aa4826b9
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-18 09:09:02 -0500

    Add DoubleDoubleRun with even cards sequence test
    This provides a unit test that ensures the # of Cards in a sequence for
    a DoubleDoubleRun with six PlayingCards equals four.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleDoubleRunTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleDoubleRunTests.swift
@@ -287,4 +287,34 @@ class DoubleDoubleRunTests: XCTestCase {
         // Then
         XCTAssertEqual(expected, actual)
     }
+    
+    /// Tests that the sequence total of a`DoubleDoubleRun`with six `PlayingCard`s equals three.
+    func test_sequence_withSixCards_equalsFour() throws {
+        
+        // Given
+        let rank1 = Rank.one
+        let rank2 = Rank.two
+        let rank3 = Rank.two
+        let rank4 = Rank.three
+        let rank5 = Rank.three
+        let rank6 = Rank.four
+        let color = Color.black
+        let symbol = Symbol.clover
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank1, of: suit)!
+        let card2 = PlayingCard(rank2, of: suit)!
+        let card3 = PlayingCard(rank3, of: suit)!
+        let card4 = PlayingCard(rank4, of: suit)!
+        let card5 = PlayingCard(rank5, of: suit)!
+        let card6 = PlayingCard(rank6, of: suit)!
+        let cards = [card1, card2, card3, card4, card5, card6]
+        let doubleDoubleRun = try DoubleDoubleRun(of: cards)
+        let expected = 4
+        
+        // When
+        let actual = doubleDoubleRun.sequence
+        
+        // Then
+        XCTAssertEqual(expected, actual)
+    }
 }
