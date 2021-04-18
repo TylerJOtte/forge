430d8e49e9898173f3fa669cb3ad3a62e7f1c9cc
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-18 10:31:19 -0500

    Add init DoubleDoubleRun with non-sequential Cards test
    This provides a unit test that ensures creating a DoubleDoubleRun with
    PlayingCards that are not in sequential order throws an Error.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleDoubleRunTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleDoubleRunTests.swift
@@ -120,6 +120,39 @@ class DoubleDoubleRunTests: XCTestCase {
         }
     }
     
+    //-------------------------------------------------------------------------//
+    //                           Non-Sequential Cards                          //
+    //-------------------------------------------------------------------------//
+    
+    /// Tests that creating a `DoubleDoubleRun` with`PlayingCards`that are not in sequential order
+    ///  throws an `ElementsError.areNotSequential Error`.
+    func test_init_withNontSequentialCards_throwsError() throws {
+
+        // Given
+        let rank1 = Rank.one
+        let rank2 = Rank.two
+        let rank3 = Rank.three
+        let rank4 = Rank.two
+        let rank5 = Rank.three
+        let color = Color.black
+        let symbol = Symbol.clover
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank1, of: suit)!
+        let card2 = PlayingCard(rank2, of: suit)!
+        let card3 = PlayingCard(rank3, of: suit)!
+        let card4 = PlayingCard(rank4, of: suit)!
+        let card5 = PlayingCard(rank5, of: suit)!
+        let cards = [card1, card2, card3, card4, card5]
+        let expected = ElementsError.areNotSequential
+        
+        // When
+        XCTAssertThrowsError(try DoubleDoubleRun(of: cards)) { error in
+            
+            // Then
+            XCTAssertEqual(expected, error as? ElementsError)
+        }
+    }
+    
     //=========================================================================//
     //                              PROPERTIES                                 //
     //=========================================================================//
