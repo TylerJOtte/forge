54d0f8fd5c102f22323bda50a5e54a1686f95507
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-18 10:55:35 -0500

    Add expected Kind max Cards test
    This provides a unit test that ensures the max Cards of a Kind equals
    the system max.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/KindTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/KindTests.swift
@@ -108,4 +108,26 @@ class KindTests: XCTestCase {
         
         XCTAssertEqual(expected, actual)
     }
+    
+    /// Tests that the max cards  of a `Kind` equals the system max
+    func test_maxCards_ofKind_equalsSystemMax() throws {
+
+        // Given
+        let rank = Rank.ace
+        let color = Color.black
+        let symbol1 = Symbol.clover
+        let symbol2 = Symbol.spade
+        let suit1 = Suit(color, symbol1)
+        let suit2 = Suit(color, symbol2)
+        let card1 = PlayingCard(rank, of: suit1)!
+        let card2 = PlayingCard(rank, of: suit2)!
+        let cards = [card1, card2]
+        let kind = try Kind(of: cards)
+        let expected = Int.max
+        
+        // When
+        let actual = kind.maxCards
+        
+        XCTAssertEqual(expected, actual)
+    }
 }
