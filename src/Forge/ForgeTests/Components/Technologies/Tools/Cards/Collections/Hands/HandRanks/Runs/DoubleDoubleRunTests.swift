ab24deb7bf57fa4faa0993c38c3f654c8f4a0c1a
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-18 09:54:59 -0500

    Add DoubleDoubleRun with insufficient pairs test
    This provides a unit test that ensures creating a DoubleDoubleRun with
    less than two pairs throws an Error.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleDoubleRunTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleDoubleRunTests.swift
@@ -26,12 +26,8 @@ class DoubleDoubleRunTests: XCTestCase {
     //=========================================================================//
     
     //-------------------------------------------------------------------------//
-    //                               Failure                                   //
+    //                            Insufficient Cards                           //
     //-------------------------------------------------------------------------//
-    
-                             //                   //
-                             // Insuffcient Cards //
-                             //                   //
     
     /// Tests that creating a `DoubleDoubleRun` with less than five `PlayingCards` throws an
     /// `ElementsError.insufficientElements Error`.
@@ -51,6 +47,39 @@ class DoubleDoubleRunTests: XCTestCase {
         let card4 = PlayingCard(rank4, of: suit)!
         let cards = [card1, card2, card3, card4]
         let expected = ElementsError.insufficientElements
+        
+        // When
+        XCTAssertThrowsError(try DoubleDoubleRun(of: cards)) { error in
+            
+            // Then
+            XCTAssertEqual(expected, error as? ElementsError)
+        }
+    }
+    
+    //-------------------------------------------------------------------------//
+    //                           Incorrect Pair Count                          //
+    //-------------------------------------------------------------------------//
+    
+    /// Tests that creating a `DoubleDoubleRun` with one pair throws an
+    /// `ElementsError.insufficientElements Error`.
+    func test_init_withZeroPairs_throwsError() throws {
+
+        // Given
+        let rank1 = Rank.one
+        let rank2 = Rank.two
+        let rank3 = Rank.two
+        let rank4 = Rank.three
+        let rank5 = Rank.four
+        let color = Color.black
+        let symbol = Symbol.clover
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank1, of: suit)!
+        let card2 = PlayingCard(rank2, of: suit)!
+        let card3 = PlayingCard(rank3, of: suit)!
+        let card4 = PlayingCard(rank4, of: suit)!
+        let card5 = PlayingCard(rank5, of: suit)!
+        let cards = [card1, card2, card3, card4, card5]
+        let expected = ElementsError.invalidDuplicateCount
         
         // When
         XCTAssertThrowsError(try DoubleDoubleRun(of: cards)) { error in
