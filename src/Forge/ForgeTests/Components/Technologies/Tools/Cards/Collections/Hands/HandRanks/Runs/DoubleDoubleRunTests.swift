f0a954c04c3f6ed53e9774810ce20f246fa451b4
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-18 08:51:37 -0500

    Add expected DoubleDoubleRun runs test
    This provides a unit test that ensures the # of runs in a
    DoubleDoubleRun equals four.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleDoubleRunTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleDoubleRunTests.swift
@@ -187,11 +187,40 @@ class DoubleDoubleRunTests: XCTestCase {
         // Then
         XCTAssertEqual(expected, actual)
     }
-
     
     //-------------------------------------------------------------------------//
     //                                Points                                   //
     //-------------------------------------------------------------------------//
     
-
+    //-------------------------------------------------------------------------//
+    //                                Runs                                     //
+    //-------------------------------------------------------------------------//
+    
+    /// Tests that the # of runs in a`DoubleDoubleRun` equals four.
+    func test_runs_ofDoubleDoubleRun_equalsFour() throws {
+        
+        // Given
+        let rank1 = Rank.one
+        let rank2 = Rank.two
+        let rank3 = Rank.two
+        let rank4 = Rank.three
+        let rank5 = Rank.three
+        let color = Color.black
+        let symbol = Symbol.clover
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank1, of: suit)!
+        let card2 = PlayingCard(rank2, of: suit)!
+        let card3 = PlayingCard(rank3, of: suit)!
+        let card4 = PlayingCard(rank4, of: suit)!
+        let card5 = PlayingCard(rank5, of: suit)!
+        let cards = [card1, card2, card3, card4, card5]
+        let doubleDoubleRun = try DoubleDoubleRun(of: cards)
+        let expected = 4
+        
+        // When
+        let actual = doubleDoubleRun.runs
+        
+        // Then
+        XCTAssertEqual(expected, actual)
+    }
 }
