e61bbdf9040ffe418a2f7eed846262413e7023f7
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-17 21:00:08 -0500

    Add expected run points of DoubleRun with five Cards test
    This provides a unit test that ensures the run points of a DoubleRun
    with five PlayingCards equals eight.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleRunTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleRunTests.swift
@@ -326,4 +326,31 @@ class DoubleRunTests: XCTestCase {
         // Then
         XCTAssertEqual(expected, actual)
     }
+    
+    /// Tests that the run points of  a`DoubleRun`with five`PlayingCard`s equals eight.
+    func test_runPoints_withFiveCards_equalsEight() throws {
+        
+        let rank1 = Rank.one
+        let rank2 = Rank.two
+        let rank3 = Rank.three
+        let rank4 = Rank.three
+        let rank5 = Rank.four
+        let color = Color.black
+        let symbol = Symbol.clover
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank1, of: suit)!
+        let card2 = PlayingCard(rank2, of: suit)!
+        let card3 = PlayingCard(rank3, of: suit)!
+        let card4 = PlayingCard(rank4, of: suit)!
+        let card5 = PlayingCard(rank5, of: suit)!
+        let cards = [card1, card2, card3, card4, card5]
+        let doubleRun = try DoubleRun(of: cards)
+        let expected = 8
+        
+        // When
+        let actual = doubleRun.runPoints
+        
+        // Then
+        XCTAssertEqual(expected, actual)
+    }
 }
