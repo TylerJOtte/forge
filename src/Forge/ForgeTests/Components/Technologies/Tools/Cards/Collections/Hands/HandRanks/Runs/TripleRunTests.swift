973059348bbbf2047192ff139deda59106cc34f9
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-18 08:44:20 -0500

    Add expected TripleRun count test
    This provides a unit that ensures the count of a TripleRun equals the #
    of Cards given.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/TripleRunTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/TripleRunTests.swift
@@ -125,6 +125,39 @@ class TripleRunTests: XCTestCase {
     }
     
     //-------------------------------------------------------------------------//
+    //                                 Count                                   //
+    //-------------------------------------------------------------------------//
+    
+    /// Tests that the count of a `TripleRun` equals the # of `PlayingCards` given.
+    func test_count_ofTripleRun_equalsNCardsGiven() throws {
+        
+        // Given
+        let rank1 = Rank.one
+        let rank2 = Rank.two
+        let rank3 = Rank.three
+        let rank4 = Rank.three
+        let rank5 = Rank.three
+        let color = Color.black
+        let symbol = Symbol.clover
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank1, of: suit)!
+        let card2 = PlayingCard(rank2, of: suit)!
+        let card3 = PlayingCard(rank3, of: suit)!
+        let card4 = PlayingCard(rank4, of: suit)!
+        let card5 = PlayingCard(rank5, of: suit)!
+        let cards = [card1, card2, card3, card4, card5]
+        let tripleRun = try TripleRun(of: cards)
+        let expected = 5
+        
+        // When
+        let actual = tripleRun.count
+        
+        // Then
+        XCTAssertEqual(expected, actual)
+    }
+    
+    
+    //-------------------------------------------------------------------------//
     //                                Title                                    //
     //-------------------------------------------------------------------------//
     
