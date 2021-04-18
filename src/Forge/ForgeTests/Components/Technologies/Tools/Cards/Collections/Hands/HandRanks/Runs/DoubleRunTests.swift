26f07ac300a48597937ac131d02b5c9af249a916
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-17 20:53:41 -0500

    Add expected DoubleRun sequence count with five Cards
    This provides a unit test that ensures the sequence count of a DoubleRun
    with five PlayingCards equals four.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleRunTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleRunTests.swift
@@ -266,4 +266,31 @@ class DoubleRunTests: XCTestCase {
         // Then
         XCTAssertEqual(expected, actual)
     }
+    
+    /// Tests that the sequence total of a`DoubleRun`with five `PlayingCard`s equals four.
+    func test_sequence_withFiveCards_equalFour() throws {
+        
+        let rank1 = Rank.one
+        let rank2 = Rank.two
+        let rank3 = Rank.three
+        let rank4 = Rank.three
+        let rank5 = Rank.four
+        let color = Color.black
+        let symbol = Symbol.clover
+        let suit = Suit(color, symbol)
+        let card1 = PlayingCard(rank1, of: suit)!
+        let card2 = PlayingCard(rank2, of: suit)!
+        let card3 = PlayingCard(rank3, of: suit)!
+        let card4 = PlayingCard(rank4, of: suit)!
+        let card5 = PlayingCard(rank5, of: suit)!
+        let cards = [card1, card2, card3, card4, card5]
+        let doubleRun = try DoubleRun(of: cards)
+        let expected = 4
+        
+        // When
+        let actual = doubleRun.sequence
+        
+        // Then
+        XCTAssertEqual(expected, actual)
+    }
 }
