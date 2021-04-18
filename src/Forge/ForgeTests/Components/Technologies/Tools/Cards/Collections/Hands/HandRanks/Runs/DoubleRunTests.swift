612e0596245dd10afda5568d86cd3cd9312cc11b
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-17 19:45:57 -0500

    Update double Run tests to use throwing constructor
    This enables each test to use the refactored throwing constructor.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleRunTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/DoubleRunTests.swift
@@ -26,20 +26,16 @@ class DoubleRunTests: XCTestCase {
     //=========================================================================//
     
     //-------------------------------------------------------------------------//
-    //                                Success                                  //
-    //-------------------------------------------------------------------------//
-    
-    
-    //-------------------------------------------------------------------------//
     //                               Failure                                   //
     //-------------------------------------------------------------------------//
     
-                            //                   //
-                            // Insuffcient Cards //
-                            //                   //
+                             //                   //
+                             // Insuffcient Cards //
+                             //                   //
 
-    /// Tests that creating a double `Run HandRank` with less than four `PlayingCards` returns nil.
-    func test_init_doubleRunWithInsufficientCards_returnsNil() throws {
+    /// Tests that creating a `DoubleRun` with less than four `PlayingCards` throws an
+    /// `ElementsError.insufficientElements Error`.
+    func test_init_withInsufficientCards_throwsError() throws {
 
         // Given
         let rank1 = Rank.ace
@@ -52,44 +48,14 @@ class DoubleRunTests: XCTestCase {
         let card2 = PlayingCard(rank2, of: suit)!
         let card3 = PlayingCard(rank3, of: suit)!
         let cards = [card1, card2, card3]
-       
-        // When
-        let doubleRun = try DoubleRun(of: cards)
-        
-        // Then
-//        XCTAssert(run == nil)
-    }
-    
-                             //                 //
-                             // Excessive Cards //
-                             //                 //
-
-    /// Tests that creating a double `Run HandRank` with more than five `PlayingCards` returns nil.
-    func test_init_doubleRunWithExcessiveCards_returnsNil() throws {
-
-        // Given
-        let rank1 = Rank.ace
-        let rank2 = Rank.two
-        let rank3 = Rank.three
-        let rank4 = Rank.three
-        let rank5 = Rank.four
-        let rank6 = Rank.five
-        let color = Color.black
-        let symbol = Symbol.clover
-        let suit = Suit(color, symbol)
-        let card1 = PlayingCard(rank1, of: suit)!
-        let card2 = PlayingCard(rank2, of: suit)!
-        let card3 = PlayingCard(rank3, of: suit)!
-        let card4 = PlayingCard(rank4, of: suit)!
-        let card5 = PlayingCard(rank5, of: suit)!
-        let card6 = PlayingCard(rank6, of: suit)!
-        let cards = [card1, card2, card3, card4, card5, card6]
+        let expected = ElementsError.insufficientElements
         
         // When
-        let doubleRun = try DoubleRun(of: cards)
-        
-        // Then
-//        XCTAssert(run == nil)
+        XCTAssertThrowsError(try DoubleRun(of: cards)) { error in
+            
+            // Then
+            XCTAssertEqual(expected, error as? ElementsError)
+        }
     }
     
     //=========================================================================//
@@ -100,9 +66,8 @@ class DoubleRunTests: XCTestCase {
     //                                Title                                    //
     //-------------------------------------------------------------------------//
 
-    
-    /// Tests that the title of a double n-`Card Run HandRank` equals "`Run` of n".
-    func test_title_ofDoubleRun_equalsDoubleRunOfN() throws {
+    /// Tests that the title of a `DoubleRun` equals "Double Run".
+    func test_title_ofDoubleRun_equalsDoubleRun() throws {
         
         let rank1 = Rank.one
         let rank2 = Rank.two
@@ -117,7 +82,7 @@ class DoubleRunTests: XCTestCase {
         let card4 = PlayingCard(rank4, of: suit)!
         let cards = [card1, card2, card3, card4]
         let doubleRun = try DoubleRun(of: cards)
-        let expected = "Double Run Of 4"
+        let expected = "Double Run"
         
         // When
         let actual = doubleRun.title
