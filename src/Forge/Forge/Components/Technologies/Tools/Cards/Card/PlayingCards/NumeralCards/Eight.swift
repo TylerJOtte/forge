380650dfd1c2c3d0a839d4c4ceca8f4fc1403e85
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-19 18:51:28 -0500

    Add Eight model
    This provides a class for standard French-suited PlayingCard Eight.

---
+++ src/Forge/Forge/Components/Technologies/Tools/Cards/Card/PlayingCards/NumeralCards/Eight.swift
@@ -0,0 +1,43 @@
+//=============================================================================//
+//                                                                             //
+//  Eight.swift                                                                //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 4/19/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import Foundation
+import SwiftUI
+
+/// A standard French-suited eight `PlayingCard`.
+public class Eight: NumeralCard {
+    
+    //=========================================================================//
+    //                               CONSTRUCTORS                              //
+    //=========================================================================//
+    
+    /// Creates an`Eight` with the given `Suit`.
+    ///
+    /// - Precondition: The given `Suit` must be `clubs`, `diamonds`, `hearts`, or
+    ///  `spades`.
+    /// - Postcondition:
+    ///   - The `Card`s `Rank`is set to `eight`.
+    ///   - The `Card`s `Suit` is set to the given `Suit`.
+    ///   - The `Card`s points are set to eight.
+    ///   - The `Card`s title is set to "`Eight` of `{Suit}`.
+    /// - Parameter suit: The symbol grouping.
+    /// - Throws: `FeatureError.invalidSuit`  if the given `Suit` is not `clubs`,
+    ///   `diamonds`, `hearts`, or `spades`.
+    public init(of suit: Suit) throws {
+        
+        try super.init(.eight, of: suit)
+    }
+}
