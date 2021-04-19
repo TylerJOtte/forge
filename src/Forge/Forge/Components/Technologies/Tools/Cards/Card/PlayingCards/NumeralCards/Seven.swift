7d280c1d8886ecb93672823320c3351d9bd063f1
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-19 18:50:14 -0500

    Add Seven model
    This provides a class for a standard French-suited PlayingCard Seven.

---
+++ src/Forge/Forge/Components/Technologies/Tools/Cards/Card/PlayingCards/NumeralCards/Seven.swift
@@ -0,0 +1,43 @@
+//=============================================================================//
+//                                                                             //
+//  Seven.swift                                                                //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 4/19/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import Foundation
+import SwiftUI
+
+/// A standard French-suited seven `PlayingCard`.
+public class Seven: NumeralCard {
+    
+    //=========================================================================//
+    //                               CONSTRUCTORS                              //
+    //=========================================================================//
+    
+    /// Creates a`Seven` with the given `Suit`.
+    ///
+    /// - Precondition: The given `Suit` must be `clubs`, `diamonds`, `hearts`, or
+    ///  `spades`.
+    /// - Postcondition:
+    ///   - The `Card`s `Rank`is set to `seven`.
+    ///   - The `Card`s `Suit` is set to the given `Suit`.
+    ///   - The `Card`s points are set to seven.
+    ///   - The `Card`s title is set to "`Seven` of `{Suit}`.
+    /// - Parameter suit: The symbol grouping.
+    /// - Throws: `FeatureError.invalidSuit`  if the given `Suit` is not `clubs`,
+    ///   `diamonds`, `hearts`, or `spades`.
+    public init(of suit: Suit) throws {
+        
+        try super.init(.seven, of: suit)
+    }
+}
