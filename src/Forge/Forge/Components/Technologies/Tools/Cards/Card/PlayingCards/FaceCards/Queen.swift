ebca00a09f801f7a1cded1aea54fde8810119e23
Tyler J. Otte <TylerJOtte@icloud.com>
2021-04-19 18:56:43 -0500

    Add Queen model
    This provides a class for a standard French-suited PlayingCard Queen.

---
+++ src/Forge/Forge/Components/Technologies/Tools/Cards/Card/PlayingCards/FaceCards/Queen.swift
@@ -0,0 +1,43 @@
+//=============================================================================//
+//                                                                             //
+//  Queen.swift                                                                //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 4/19/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import Foundation
+import SwiftUI
+
+/// A standard French-suited queen `PlayingCard`.
+public class Queen: FaceCard {
+    
+    //=========================================================================//
+    //                               CONSTRUCTORS                              //
+    //=========================================================================//
+    
+    /// Creates a`Queen` with the given `Suit`.
+    ///
+    /// - Precondition: The given `Suit` must be `clubs`, `diamonds`, `hearts`, or
+    ///  `spades`.
+    /// - Postcondition:
+    ///   - The `Card`s `Rank`is set to `queen`.
+    ///   - The `Card`s `Suit` is set to the given `Suit`.
+    ///   - The `Card`s points are set to 10.
+    ///   - The `Card`s title is set to "`Queen` of `{Suit}`.
+    /// - Parameter suit: The symbol grouping.
+    /// - Throws: `FeatureError.invalidSuit`  if the given `Suit` is not `clubs`,
+    ///   `diamonds`, `hearts`, or `spades`.
+    public init(of suit: Suit) throws {
+        
+        try super.init(.queen, of: suit)
+    }
+}
