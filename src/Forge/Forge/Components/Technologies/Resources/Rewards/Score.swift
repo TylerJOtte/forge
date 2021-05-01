315e329a55c9f56466a8bdc667aed279683b9160
Tyler J. Otte <TylerJOtte@icloud.com>
2021-05-01 12:16:35 -0500

    Add Score model
    This provides a class for a point total.

---
+++ src/Forge/Forge/Components/Technologies/Resources/Rewards/Score.swift
@@ -0,0 +1,24 @@
+//=============================================================================//
+//                                                                             //
+//  Score.swift                                                                //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 5/01/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import Foundation
+
+/// A point total.
+public class Score: Reward, Scoreable {
+    
+    /// The total # of points.
+    public var points: Int { return 0 }
+}
