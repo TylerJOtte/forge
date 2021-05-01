c036918982eed6ccda1e06c365a4eb300f9e84f7
Tyler J. Otte <TylerJOtte@icloud.com>
2021-05-01 11:37:59 -0500

    Add Scoreable protocol
    This provides a protocol for a scoreable characteristic.

---
+++ src/Forge/Forge/System/Traits/Scoreable.swift
@@ -0,0 +1,24 @@
+//=============================================================================//
+//                                                                             //
+//  Scoreable.swift                                                            //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 5/01/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import Foundation
+
+/// A scoreable characteristic.
+public protocol Scoreable {
+    
+    /// The total # of points.
+    var points: Int { get }
+}
