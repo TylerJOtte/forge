53096dac9196f745306362b31b21b42d9baebedf
Tyler J. Otte <TylerJOtte@icloud.com>
2021-05-14 18:50:48 -0500

    Add ElementError model
    This provides a game Element Error "interface".

---
+++ src/Forge/Forge/System/Errors/ElementError.swift
@@ -0,0 +1,13 @@
+//
+//  ElementError.swift
+//  Forge
+//
+//  Created by Tyler J. Otte on 5/12/21.
+//
+
+import Foundation
+
+/// A game `Element Error`.
+public protocol ElementError: Element, Error {
+
+}
