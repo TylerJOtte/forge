65dfdc22a1e37e9865545be56d0e8684a077445b
Tyler J. Otte <TylerJOtte@icloud.com>
2021-05-16 12:24:16 -0500

    Add SuitExtension is red tests
    This provides unit test that ensure only hearts and diamonds Suits are
    red PlayingCard Suits.

---
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Types/PlayingCards/Extensions/SuitExtension.swift
@@ -0,0 +1,98 @@
+//=============================================================================//
+//                                                                             //
+//  SuitExtensionTests.swift                                                   //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 5/16/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import XCTest
+@testable import Forge
+
+/// Unit tests for a `PlayingCard Suit`.
+class SuitExtensionTests: XCTestCase {
+    
+    //=========================================================================//
+    //                                 TESTERS                                 //
+    //=========================================================================//
+    
+    //-------------------------------------------------------------------------//
+    //                                  isRed                                  //
+    //-------------------------------------------------------------------------//
+    
+    //              //
+    // PlayingCards //
+    //              //
+    
+    /// Tests that a `hearts Suit` is a `red PlayingCard Suit`.
+    func test_hearts_isRed_true() {
+        
+        // Given
+        let suit = Suit.hearts
+        
+        // When/Then
+        XCTAssert(suit.isRed())
+    }
+    
+    /// Tests that a `diamonds Suit` is a `red PlayingCard Suit`.
+    func test_diamonds_isRed_true() {
+        
+        // Given
+        let suit = Suit.diamonds
+        
+        // When/Then
+        XCTAssert(suit.isRed())
+    }
+    
+    /// Tests that a `clubs Suit` is not a `red PlayingCard Suit`.
+    func test_clubs_isRed_false() {
+        
+        // Given
+        let suit = Suit.clubs
+        
+        // When/Then
+        XCTAssertFalse(suit.isRed())
+    }
+    
+    /// Tests that a `spades Suit` is not a `red PlayingCard Suit`.
+    func test_spades_isRed_false() {
+        
+        // Given
+        let suit = Suit.spades
+        
+        // When/Then
+        XCTAssertFalse(suit.isRed())
+    }
+    
+    /// Tests that a `null Suit` is not a `red PlayingCard Suit`.
+    func test_null_isRed_false() {
+        
+        // Given
+        let suit = Suit.null
+        
+        // When/Then
+        XCTAssertFalse(suit.isRed())
+    }
+    
+    //                 //
+    // Non-PlayingCard //
+    //                 //
+    
+    /// Tests that a non-`PlayingCard Suit` is not a `red PlayingCard Suit`.
+    func test_nonPlayingCardSuit_isRed_false() {
+        
+        // Given
+        let suit = Suit.stars
+        
+        // When/Then
+        XCTAssertFalse(suit.isRed())
+    }
+}
