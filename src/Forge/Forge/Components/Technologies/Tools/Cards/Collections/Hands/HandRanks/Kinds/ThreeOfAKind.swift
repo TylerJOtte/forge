8b83a4a247e6bb13f57a1d60bbf8bd1c54580686
Tyler J. Otte <TylerJOtte@icloud.com>
2021-05-16 16:38:56 -0500

    Add ThreeOfAKind model
    This provides a class for a three of a Kind RankedCard collection.

---
+++ src/Forge/Forge/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Kinds/ThreeOfAKind.swift
@@ -0,0 +1,43 @@
+//=============================================================================//
+//                                                                             //
+//  ThreeOfAKind.swift                                                         //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 5/16/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import Foundation
+
+/// A `Kind` of three equally `Rank`ed `Card`s.
+public class ThreeOfAKind: Kind {
+    
+    //=========================================================================//
+    //                               CONSTRUCTORS                              //
+    //=========================================================================//
+    
+    /// Creates a`ThreeOfAKind`with the given `Card`s.
+    ///
+    /// - Precondition: The given `Card`s must all contain the same `Rank`.
+    /// - Postcondition:
+    ///   - The `ThreeOfAKind` contains the given `Card`s.
+    ///   - The `ThreeOfAKind`'s points are set to 6.
+    ///   - The `ThreeOfAKind` can only hold three ` Card`s.
+    ///   - The `ThreeOfAKind`'s title is set to "Three Of A Kind".
+    /// - Parameter cards: The `Card`s to include in the `ThreeOfAKind`.
+    /// - Throws: `invalidRank` if the given `Card`s do not all contain the same `Rank`.
+    public init(_ cards: [RankedCard]) throws {
+
+        let min = 3
+        let max = 3
+
+        try super.init(of: min, to: max, cards)
+    }
+}
