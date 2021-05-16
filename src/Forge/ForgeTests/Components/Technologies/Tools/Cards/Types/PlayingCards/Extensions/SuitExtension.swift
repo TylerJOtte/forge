f2bd99335e3d99d25a97ab419f072692c347ff29
Tyler J. Otte <TylerJOtte@icloud.com>
2021-05-16 12:28:17 -0500

    Add SuitExtension is black tests
    This provides unit tests that ensure only clubs and spades Suits are
    black PlayingCard Suits.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Types/PlayingCards/Extensions/SuitExtension.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Types/PlayingCards/Extensions/SuitExtension.swift
@@ -95,4 +95,77 @@ class SuitExtensionTests: XCTestCase {
         // When/Then
         XCTAssertFalse(suit.isRed())
     }
+    
+    //-------------------------------------------------------------------------//
+    //                                 isBlack                                 //
+    //-------------------------------------------------------------------------//
+    
+    //              //
+    // PlayingCards //
+    //              //
+    
+    /// Tests that a `clubs Suit` is a `black PlayingCard Suit`.
+    func test_clubs_isBlack_true() {
+        
+        // Given
+        let suit = Suit.clubs
+        
+        // When/Then
+        XCTAssert(suit.isBlack())
+    }
+    
+    /// Tests that a `spades Suit` is a `black PlayingCard Suit`.
+    func test_spades_isBlack_true() {
+        
+        // Given
+        let suit = Suit.spades
+        
+        // When/Then
+        XCTAssert(suit.isBlack())
+    }
+    
+    /// Tests that a `hearts Suit` is not a `black PlayingCard Suit`.
+    func test_hearts_isBlack_false() {
+        
+        // Given
+        let suit = Suit.hearts
+        
+        // When/Then
+        XCTAssertFalse(suit.isBlack())
+    }
+    
+    /// Tests that a `diamonds Suit` is not a `black PlayingCard Suit`.
+    func test_diamonds_isBlack_false() {
+        
+        // Given
+        let suit = Suit.diamonds
+        
+        // When/Then
+        XCTAssertFalse(suit.isBlack())
+    }
+    
+    /// Tests that a `null Suit` is not a `black PlayingCard Suit`.
+    func test_null_isBlack_false() {
+        
+        // Given
+        let suit = Suit.null
+        
+        // When/Then
+        XCTAssertFalse(suit.isBlack())
+    }
+    
+    //                 //
+    // Non-PlayingCard //
+    //                 //
+    
+    /// Tests that a non-`PlayingCard Suit` is not a `black PlayingCard Suit`.
+    func test_nonPlayingCardSuit_isBlack_false() {
+        
+        // Given
+        let suit = Suit.stars
+        
+        // When/Then
+        XCTAssertFalse(suit.isBlack())
+    }
+    
 }
