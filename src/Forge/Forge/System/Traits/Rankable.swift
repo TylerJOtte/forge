e58a742c2e3438f6c16e33afa0d364dd5dc71109
Tyler J. Otte <TylerJOtte@icloud.com>
2021-05-16 14:58:46 -0500

    Add Rankable protocol
    This provides a contract for a Rankable model.

---
+++ src/Forge/Forge/System/Traits/Rankable.swift
@@ -0,0 +1,23 @@
+//=============================================================================//
+//                                                                             //
+//  Rankable.swift                                                             //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 5/16/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+
+/// A rankable `Trait`.
+public protocol Rankable: Orderable {
+    
+    /// The hierarchical position.
+    var rank: Rank { get }
+}
