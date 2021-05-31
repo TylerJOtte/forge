a6e370251d4a351ba41444a390efa25e8d94c94b
Tyler J. Otte <TylerJOtte@icloud.com>
2021-05-31 15:47:50 -0500

    Add ComponentError model
    This provides a protocol for a throwable Component Error.

---
+++ src/Forge/Forge/System/Errors/Components/ComponentError.swift
@@ -0,0 +1,22 @@
+//=============================================================================//
+//                                                                             //
+//  ComponentError.swift                                                      //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 5/30/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import Foundation
+
+/// A throwable `Component Error`.
+public protocol ComponentError: ElementError {
+    
+}
