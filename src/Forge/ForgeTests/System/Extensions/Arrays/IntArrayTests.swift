e1a4d5d3752bc1fc1bf579b2f82fb8f30009bff5
Tyler J. Otte <TylerJOtte@icloud.com>
2021-06-11 18:53:22 -0500

    Add sum numbers in Array test
    This provides a unit test that ensures summing the points of numbers in
    an Array of Ints equals an expected value.

---
+++ src/Forge/ForgeTests/System/Extensions/Arrays/IntArrayTests.swift
@@ -0,0 +1,45 @@
+//=============================================================================//
+//                                                                             //
+//  IntArrayTests.swift                                                        //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 6/02/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import XCTest
+@testable import Forge
+
+/// Unit tests for an `Int Array`.
+class IntArrayTests: XCTestCase {
+ 
+    //=========================================================================//
+    //                               CALCULATORS                               //
+    //=========================================================================//
+    
+    //-------------------------------------------------------------------------//
+    //                               sumPoints()                               //
+    //-------------------------------------------------------------------------//
+    
+    /// Tests that summing all the numbers in an `Int Array` equals an expected value.
+    func test_sumPoints_ofNumbers_equalsExpected() throws {
+        
+        // Given
+        let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        let expected = 55
+        
+        // When
+        let actual = numbers.sum()
+        
+        // Then
+        XCTAssertEqual(expected, actual)
+    }
+}
+    
