6772ed1b4778dbcdd66abeb6d91decdf8db4b968
Tyler J. Otte <TylerJOtte@icloud.com>
2021-06-11 17:53:44 -0500

    Add HandRank Array Extension
    This provides an extension for common HandRank Array operations.

---
+++ src/Forge/Forge/System/Extensions/Arrays/HandRankArray.swift
@@ -0,0 +1,54 @@
+//=============================================================================//
+//                                                                             //
+//  HandRankArray.swift                                                        //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 6/06/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+
+/// An extension for common `HandRank<RankedCard> Array` operations.
+extension Array where Element: HandRank<RankedCard> {
+    
+    /// Determines if contains all the unique `RankedCard`s in the given `Pair Array`.
+    ///
+    /// - Precondition: None.
+    /// - Postcondition: None.
+    /// - Returns: True if contains all the unique `RankedCard`s in the `Pair Array`, else false.
+    func containCards(in pairs: [Pair<RankedCard>]) -> Bool {
+     
+        var containsPairCards = true
+        var handRank = 0
+        
+        while (containsPairCards && handRank < count) {
+            
+            containsPairCards = self[handRank].containsCard(in: pairs)
+            handRank += 1
+        }
+        
+        return containsPairCards
+    }
+}
+
+/// An extension for common `Array<HandRank<RankedCard>> Array` operations.
+extension Array where Element == Array<HandRank<RankedCard>> {
+
+    /// Sums all the `Element`s' points.
+    ///
+    /// - Precondition: None.
+    /// - Postcondition: None.
+    /// - Returns: The sum of all the `Element`s' points.
+    func sumPoints() -> Int {
+        
+        return self.map{$0.sumPoints()}.reduce(0, +)
+    }
+}
+
