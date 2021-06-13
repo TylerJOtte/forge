0ac7f3cdb47df3a25c6fbd4ec1b2249d09d2ec5c
Tyler J. Otte <TylerJOtte@icloud.com>
2021-06-13 14:14:09 -0500

    Add init MultiRun with invalid # of Pairs tests
    This provides unit test that ensure creating a MultiRun with zero Pairs
    and excessive Pairs throws an invalidKindCount Error.

--- src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/MultiRuns/MultiRunTests.swift
+++ src/Forge/ForgeTests/Components/Technologies/Tools/Cards/Collections/Hands/HandRanks/Runs/MultiRuns/MultiRunTests.swift
@@ -49,4 +49,54 @@ class MultiRunTests: XCTestCase {
             XCTAssertEqual(expected, error as? RangeError)
         }
     }
+    
+    //-------------------------------------------------------------------------//
+    //                          Invalid Pair Count                             //
+    //-------------------------------------------------------------------------//
+
+    /// Tests that creating a `MultiRun` with zero `Pair`s throws an `invalidKindCount Error`.
+    func test_init_withZeroPairs_throwsInvalidKindCountError() throws {
+
+        // Given
+        let min = 4
+        let ten = try Ten(of: .hearts)
+        let jack = try Jack(of: .hearts)
+        let queen = try Queen(of: .hearts)
+        let king = try King(of: .hearts)
+        let cards = [ten, jack, queen, king]
+        let count = 2
+        let expected = HandRankError.invalidKindCount
+
+        // When
+        XCTAssertThrowsError(try MultiRun(of: min, cards, with: count)) {
+            error in
+
+            // Then
+            XCTAssertEqual(expected, error as? HandRankError)
+        }
+    }
+    
+    /// Tests that creating a `MultiRun` with more `Pair`s than expected throws an
+    /// `invalidKindCount Error`.
+    func test_init_witExcessivePairs_throwsInvalidKindCountError() throws {
+
+        // Given
+        let min = 4
+        let jack = try Jack(of: .hearts)
+        let queen1 = try Queen(of: .hearts)
+        let queen2 = try Queen(of: .spades)
+        let king1 = try King(of: .hearts)
+        let king2 = try King(of: .spades)
+        let cards = [jack, queen1, queen2, king1, king2]
+        let count = 2
+        let expected = HandRankError.invalidKindCount
+
+        // When
+        XCTAssertThrowsError(try MultiRun(of: min, cards, with: count)) {
+            error in
+
+            // Then
+            XCTAssertEqual(expected, error as? HandRankError)
+        }
+    }
 }
