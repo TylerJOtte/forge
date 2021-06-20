5ad012b1a41a68531e3f489ab1aa81fb81554649
Tyler J. Otte <TylerJOtte@icloud.com>
2021-06-20 10:20:32 -0500

    Add split on capitals with titlecased SubSequence
    This provides a unit test that ensures splitting on the capitals with a
    titlecased String.SubSequence returns a String with a space before each
    capital letter by default.

---
+++ src/Forge/ForgeTests/System/Extensions/Sequences/StringSubSequenceExtensionTests.swift
@@ -0,0 +1,45 @@
+//=============================================================================//
+//                                                                             //
+//  StringSubSequenceExtensionTests.swift                                      //
+//  Forge                                                                      //
+//                                                                             //
+//  Created by Tyler J. Otte on 6/20/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the Forge framework project.                    //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/forge/LICENSE.txt for more details.       //
+//=============================================================================//
+
+import XCTest
+@testable import Forge
+
+/// Unit tests for a `String.SubSequence extension`.
+class StringSubSequenceExtensionTests: XCTestCase {
+ 
+    //=========================================================================//
+    //                             Initialization                              //
+    //=========================================================================//
+    
+    //-------------------------------------------------------------------------//
+    //                               sumPoints()                               //
+    //-------------------------------------------------------------------------//
+
+    /// Tests that splitting on the capitals with a titlecased `String.SubSequence` returns a `String`
+    /// with a space before each capital letter by default.
+    func test_splitOnCapitals_withTitlecasedValue_returnsSpacesBeforeCapitals() {
+        
+        // Given
+        let subSequence: String.SubSequence = "FourOfAKind"
+        let expected = "Four Of A Kind"
+        
+        // When
+        let actual = subSequence.splitOnCapitals()
+        
+        // Then
+        XCTAssertEqual(expected, actual)
+    }
+}
