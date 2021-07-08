282197bd222245d9f892f256762db7a4b2f77e9f
Tyler J. Otte <TylerJOtte@icloud.com>
2021-07-07 19:18:33 -0500

    Add totalCount with empty/non-empty Elements expected test
    This provides a unit test that ensures the totalCount of a Collection
    with empty and non-empty Elements equals an expected value.

--- src/Forge/ForgeTests/System/Extensions/Collections/ElementsCollection.swift
+++ src/Forge/ForgeTests/System/Extensions/Collections/ElementsCollection.swift
@@ -28,8 +28,12 @@ class ElementsCollectionTests: XCTestCase {
     //                                totalCount                               //
     //-------------------------------------------------------------------------//
     
+    //                       //
+    // With Empty Collection //
+    //                       //
+    
     /// Tests that the total count of an empty `Collection` equals zero.
-    func test_totalCount_ofEmptyCollection_equalsZero() {
+    func test_totalCount_withEmptyCollection_equalsZero() {
         
         // Given
         let collection: [Hand] = []
@@ -42,12 +46,37 @@ class ElementsCollectionTests: XCTestCase {
         XCTAssertEqual(expected, actual)
     }
     
-    /// Tests that the total count of a `Collection` with all empty `Elements` equals zero.
-    func test_totalCount_ofCollectionWithAllEmptyElements_equalsZero() {
+    //                     //
+    // With Empty Elements //
+    //                     //
+    
+    /// Tests that the total count with all empty `Elements` equals zero.
+    func test_totalCount_withEmptyElements_equalsZero() {
         
         // Given
         let collection = [Hand(), Hand(), Hand()]
         let expected = 0
+        
+        // When
+        let actual = collection.totalCount
+        
+        // Then
+        XCTAssertEqual(expected, actual)
+    }
+    
+    //                                   //
+    // With Empty And Non-Empty Elements //
+    //                                   //
+    
+    /// Tests that the total count with empty and non-empty `Elements` equals an expected value.
+    func test_totalCount_withEmptyAndNonEmptyElements_equalsExpected() {
+        
+        // Given
+        let elements1 = Hand(of: [Card()])
+        let elements2 = Hand()
+        let elements3 = Hand(of: [Card(), Card(), Card()])
+        let collection = [elements1, elements2, elements3]
+        let expected = 4
         
         // When
         let actual = collection.totalCount
